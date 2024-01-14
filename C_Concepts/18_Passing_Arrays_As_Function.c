d64d8a213bf190ab2991f4ab6569874ae52dbe69
Nil369 <148447931+Nil369@users.noreply.github.com>
2024-01-15 02:14:36 +0530

    added parsing array in a function & string_into

---
+++ C_Concepts/18_Passing_Arrays_As_Function.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int func1(int array[])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("The value at %d is %d\n", i, array[i]);
+    }
+    // array[0] = 382; // Very important point that if you change any value here, it gets reflected in main()
+    return 0;
+}
+
+
+void func2(int *ptr)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("The value at %d is %d\n", i, *(ptr + i));
+    }
+    *(ptr + 2) = 6534;
+}
+
+
+void func3(int arr[2][2])
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("The value at %d, %d is %d\n", i, j, arr[i][j]);
+        }
+    }
+}
+
+
+int main()
+{   
+    int arr1[] = {23,13,3,4};
+    printf("The value at index 0 is %d\n", arr1[0]);
+    func1(arr1);
+    printf("The value at index 0 is %d\n", arr1[0]);
+    func2(arr1);
+    func2(arr1); 
+    int arr[][2] = {{2, 13}, {9, 3}};
+    
+    func3(arr);
+    return 0;
+}
