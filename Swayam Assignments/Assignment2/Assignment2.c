d4b783ef01cbac0e5dc8fbdd4516405fbafed5dc
Nil369 <148447931+Nil369@users.noreply.github.com>
2024-02-04 23:20:21 +0530

    Solved 2 more Assignments

---
+++ Swayam Assignments/Assignment2/Assignment2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x=4,y=5;
+    if (x==4 && y==5)
+    {
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+}
---
+++ Swayam Assignments/Assignment3/Assignment3.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main(){
+
+// Q2. Output of this code:
+    // float i = -3.0;
+    // int k = i % 2;
+    // printf("%d",k);
+    // Ans: Compilation Error
+
+//Q3. Output of the code:
+    /*int a=10,b=3,c=2,d=4,result;
+    result=a+a*-b/c%d+c*d;
+    printf("%d",result);*/
+    // Ans = 15
+
+// Q4.Output of code:
+
+// int h,b;
+// b = 4*6+3*4<h*5?4:3;
+// printf("%d\n",b);
+
+// Q5. Ans = False
+// int a=1,b=2,c=3;
+// if (c>b>a)
+// {
+//     printf("TRUE");
+// }
+// else{
+//     printf("FALSE");
+// }
+
+//Q6.Output of the code:
+// int x=1;
+// if ((3>5)||(2!=3))
+// {
+//     printf("IITKGP\n");
+// }
+// else if(x&=0){
+//     printf("IITD\n");
+// }else{printf("IITM \n");}
+
+
+//Q7. Output of the code:
+    //  int x=2;
+    //  if (x=1) // Assigning the value 1 => True (Ans)
+    //  {
+    //     printf("TRUE");
+    //  }
+    //  else{printf("FALSE");}
+
+
+// Q10 .  
+     // int x=9<5+3&&7;
+    // printf("%d",x); Ans: 0 due to the comparison & Logical && operator
+
+    return 0;
+}
