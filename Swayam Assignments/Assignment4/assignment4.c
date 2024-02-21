e1d14ed0c7b520fcafcadac7e64b13e84ed562b3
Akash Halder <halderakash872@gmail.com>
2024-02-21 23:53:07 +0530

    solved swayam assignment no.4

---
+++ Swayam Assignments/Assignment4/assignment4.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+int main(){
+    // Q1. what will be the value of a
+    // int a=5,b=10;
+    // a+=a<=b;
+    // printf("%d",a);
+
+    // int x=5, y=3;
+    // printf("%d",x>y?x++:y++);
+
+    // int a =10;
+    // while (a-- > 10)
+    // {
+    //     printf("%d",a);
+    // }
+
+    // int x=1, y=2;
+    // printf("%d",x<y?x:y, x>y?y:x);
+    
+    return 0;
+}
