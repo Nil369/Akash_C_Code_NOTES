eeaf2035a20258059f8145e86719ba71b7322472
Akash Halder <halderakash872@gmail.com>
2024-04-25 00:17:38 +0530

    added memory allocations

---
+++ C_Concepts/23_Dynamic_Memory_location.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h> //
+
+int main()
+{
+    // 1. Use of malloc:
+
+
+    // int *ptr;
+    // int n;
+    // printf("Enter the size of the array you want to create\n");
+    // scanf("%d", &n);
+
+    // ptr = (int *)malloc(n * sizeof(int));
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("Enter the value no %d of this array\n",i);
+    //    scanf("%d", &ptr[i]);
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("The value at %d of this array is %d\n",i, ptr[i]);
+    // }
+
+    //2. Use of calloc:
+
+    int *ptr;
+    int n;
+    printf("Enter the size of the array you want to create\n");
+    scanf("%d", &n);
+
+    ptr = (int *)calloc(n ,  sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the value no %d of this array\n",i);
+       scanf("%d", &ptr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("The value at %d of this array is %d\n",i, ptr[i]);
+    }
+
+    //Use of realloc: 
+    
+    printf("Enter the size of the new array you want to create\n");
+    scanf("%d", &n);
+
+    ptr = (int *)realloc(ptr ,  n*sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the new value no %d of this array\n",i);
+       scanf("%d", &ptr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("The new value at %d of this array is %d\n",i, ptr[i]);
+    }
+
+    free(ptr);
+    
+    
+    return 0;
+}
